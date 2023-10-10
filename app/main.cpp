97653b2c4926c6c7ce4b356e4e7f92e3f74517d9
TopHatTacco <105398626+TopHatTacco@users.noreply.github.com>
2023-10-10 13:29:01 -0600

    comments, credits, and randomness
    Added comments to everything so that it is easier to follow, including a credits section at the top. Also now using srand(time(0)) to generate a random seed based on Unit timestamps. This fixes the issue with the randomness that was in the "known issues" section of the readme.md. Removed that part.
    
    PS: also changes some things to say team 3 instead of team 4 bc for some reason I thought we were team 4.

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,30 @@
+/* -- EIGHT BALL --
+
+    COP-1000 2023-2024 project by: 
+
+    Michael Frank 
+    Michael Grigsby 
+    Aidan McAteer
+    Amaris McCalla
+    Kaylee Swain
+
+    Created by Team 3
+*/
+
+//import libraries
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <time.h> 
 
+// set up base varables
 std::string userInp;
 std::string chosenPrefix;
 std::string chosenAnswer;
 const int answerNum = 6;
 const int prefixNum = 8;
 
+// array of all possible answers
 std::string answer[answerNum] = {"yes ", 
                         "no ",
                         "probably ", 
@@ -15,6 +32,7 @@ std::string answer[answerNum] = {"yes ",
                         "[REDACTED] ",
                         "yo mama "};
 
+// array of all possible prefixes
 std::string prefix[prefixNum] = {"my pet hampster says ", 
                         //"I rolled a dice and it said " + str(diceResult) + " so im taking that as a ", 
                         "taking a random guess here but ", 
@@ -25,13 +43,19 @@ std::string prefix[prefixNum] = {"my pet hampster says ",
                         "I think you already know the answer, but ", 
                         "That's your question? You have acess to the most accurate predictor mankind has ever produced and you asked that? Fine. The answer is "};
 
+// declare the create response function
 std::string createResponse();
 
-
+// MAIN
 int main() {
 
+    // start question prompt loop
     while (true)
     {  
+        // set the rand seed to be based on Unix time (helps make the randomness more random)
+        srand(time(0));
+
+        // prompt and response system
         std::cout << "what is your question? : ";
         std::cin >> userInp;
         std::string response = createResponse();
@@ -39,14 +63,20 @@ int main() {
     }
 }
 
+// function to generte a unique response to the quesiton
 std::string createResponse()
 {
+    // generate a random number based on the number of elements in both answer and prefix arrays. Used as the index for getting a string.
     int randPreChoice = (rand() % prefixNum);
     int randAnsChoice = (rand() % answerNum);
 
-
+    // get a random string from both the prefix array and the answer array, used to create the final response
     chosenPrefix = prefix[randPreChoice];
     chosenAnswer = answer[randAnsChoice];
+
+    // create response by combining the profix and suffix strings
     std::string response = chosenPrefix + chosenAnswer;
+
+    // return the final response so it can be outputted in MAIN
     return response;
 }
